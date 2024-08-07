507c385a5d2932565be021a356d322fda8c3789b
sinedied <noda@free.fr>
2024-08-07 15:50:32 +0200

    feat: initial playground implementation

--- src/webapp/src/app/session.component.ts
+++ src/webapp/src/app/session.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-session',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      session works!
-    </p>
+    <section>
+      <p>Current session ID: {{ session.sessionId() }}</p>
+      <p>
+        <button (click)="newSession()" [disabled]="wait()">New session</button>
+      </p>
+    </section>
   `,
   styles: ``
 })
 export class SessionComponent {
+  session = inject(SessionService);
+  wait = signal<boolean>(false);
 
+  async newSession() {
+    this.wait.set(true);
+    const sessionId = await this.session.createNewSession();
+    this.wait.set(false);
+  }
 }
--- src/webapp/src/app/session.service.ts
+++ src/webapp/src/app/session.service.ts
@@ -1,21 +1,73 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 const api = 'http://localhost:7071/api';
+
+export interface RemoteFile {
+  $id: string;
+  filename: string;
+  size: number;
+  last_modified_time: string;
+}
+
+export interface RunOutput {
+  result: string;
+  stdout: string;
+  stderr: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-  async runCode(code: string) {
+  sessionId = signal<string>('');
+
+  async createNewSession(): Promise<string> {
+    const sessionId = uuidv4();
+    await this.fetchJson(`${api}/sessions/${sessionId}`, {
+      method: 'POST',
+      body: JSON.stringify({ code: 'true' }),
+    });
+    this.sessionId.set(sessionId);
+    return sessionId;
   }
 
-  async uploadFile(name: string, data: Blob) {
-
+  async runCode(code: string): Promise<RunOutput> {
+    return this.fetchJson(`${api}/sessions/${this.sessionId()}`, {
+      method: 'POST',
+      body: JSON.stringify({ code }),
+    });
   }
 
-  async downaloadFile(name: string) {
+  async uploadFile(name: string, data: Blob): Promise<RemoteFile> {
+    return this.fetchJson(`${api}/sessions/${this.sessionId()}/files`, {
+      method: 'POST',
+      body: JSON.stringify({ name, data }),
+    });
   }
 
-  async listFiles() {
+  async downloadFile(name: string): Promise<string> {
+    const result = await fetch(`${api}/sessions/${this.sessionId()}/files/${name}`);
+    if (!result.ok) {
+      throw new Error(`Failed to call API: ${result.statusText}`);
+    }
+    return result.text();
   }
+
+  async listFiles(): Promise<RemoteFile[]> {
+    return this.fetchJson(`${api}/sessions/${this.sessionId()}/files`);
+  }
+
+  private async fetchJson(url: string, options?: RequestInit) {
+    const result = await fetch(url, options);
+    if (!result.ok) {
+      throw new Error(`Failed to call API: ${result.statusText}`);
+    }
+    return result.json();
+  }
+}
+
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
 }
